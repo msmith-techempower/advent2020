e5d70dcd90bc5a55d96a21d519261c83d36fe233
Mike Smith <msmith@techempower.com>
2020-12-01 11:13:23 -0800

    Use HashSet instead of dumb nested loops

--- src/exercises/day1/mod.rs
+++ src/exercises/day1/mod.rs
@@ -1,70 +1,56 @@
 use std::fs::File;
 use std::io::Read;
+use std::collections::HashSet;
 
-fn read_input() -> Vec<u32> {
+fn _read_input() -> HashSet<u32> {
     let mut file = File::open("src/exercises/day1/input.txt").expect("File exists and can be opened");
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("Can read and is valid text");
 
-    let mut numbers = vec![];
+    let mut numbers = HashSet::default();
     for input in contents.lines() {
-        numbers.push(str::parse::<u32>(input).expect("It is a u32"));
+        numbers.insert(str::parse::<u32>(input).expect("It is a u32"));
     }
 
     numbers
 }
 
-/// `fn find_two_that_sum_to_2020() -> (num_a, num_b, sum)`
-/// where `num_a * num_b == 2020`
-fn find_two_that_sum_to_2020() -> (u32, u32, u32) {
-    let numbers = read_input();
-
-    let mut index = 0;
-    while index < numbers.len() {
-        let number_a = numbers[index];
-        // No reason to check the beginning again.
-        let rest = &numbers[index+1..];
-        // // SAFETY: no number in the input is > 2020, so this won't underflow
-        for number_b in rest {
-            if 2020 - number_a == *number_b {
-                return (number_a, *number_b, number_a * number_b);
-            }
+fn _find_two_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u32, u32)> {
+    for number in numbers {
+        if number > &sum_to {
+            continue;
         }
-        index += 1;
-    }
-
-    panic!("Found no numbers that sum to 2020!");
-}
-
-/// `fn find_two_that_sum_to_2020() -> (num_a, num_b, num_c, sum)`
-/// where `num_a + num_b + num_c == 2020`
-fn find_three_that_sum_to_2020() -> (u32, u32, u32, u32) {
-    let numbers = read_input();
-
-    // Performance is bad and I should feel bad.
-    for number_a in &numbers {
-        for number_b in &numbers {
-            for number_c in &numbers {
-                if number_a + number_b + number_c == 2020 {
-                    // ... but I don't.
-                    return (*number_a, *number_b, *number_c, number_a * number_b * number_c);
-                }
-            }
+        // SAFETY: no number in the input is > 2020, so this won't underflow
+        let pair = sum_to - number;
+        if numbers.contains(&pair) {
+            return Some((*number, pair, number * pair));
         }
     }
 
-
-
-    panic!("Found no numbers that sum to 2020!");
+    None
 }
+
+fn _find_three_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u32, u32, u32)> {
+    for number in numbers {
+        // SAFETY: no number in the input is > 2020, so this won't underflow
+        let rest = sum_to - number;
+        if let Some((number_b, number_c, product)) = _find_two_that_sum_to(&numbers, rest) {
+            return Some((*number, number_b, number_c, product * number));
+        }
+    }
+
+    None
+}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::exercises::day1::{find_two_that_sum_to_2020, find_three_that_sum_to_2020};
+    use crate::exercises::day1::{_find_two_that_sum_to, _read_input, _find_three_that_sum_to};
 
     #[test]
     fn day_1a_works() {
-        let output = find_two_that_sum_to_2020();
+        let numbers = _read_input();
+        let output = _find_two_that_sum_to(&numbers, 2020).expect("There to be two such numbers");
         assert_eq!(output.0 + output.1, 2020);
         assert_eq!(output.0 * output.1, output.2);
         eprintln!("Output 1a: {}", output.2);
@@ -72,7 +58,8 @@ mod tests {
 
     #[test]
     fn day_1b_works() {
-        let output = find_three_that_sum_to_2020();
+        let numbers = _read_input();
+        let output = _find_three_that_sum_to(&numbers, 2020).expect("There to be three such numbers");
         assert_eq!(output.0 + output.1 + output.2, 2020);
         assert_eq!(output.0 * output.1 * output.2, output.3);
         eprintln!("Output 1b: {}", output.3);
