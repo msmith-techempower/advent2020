95d38e3fc7d7d36934da66646fe0fb9985713354
Mike Smith <msmith@techempower.com>
2020-12-01 11:30:47 -0800

    Only reading in tests

--- src/exercises/day1/mod.rs
+++ src/exercises/day1/mod.rs
@@ -1,19 +1,4 @@
-use std::fs::File;
-use std::io::Read;
 use std::collections::HashSet;
-
-fn _read_input() -> HashSet<u32> {
-    let mut file = File::open("src/exercises/day1/input.txt").expect("File exists and can be opened");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).expect("Can read and is valid text");
-
-    let mut numbers = HashSet::default();
-    for input in contents.lines() {
-        numbers.insert(str::parse::<u32>(input).expect("It is a u32"));
-    }
-
-    numbers
-}
 
 fn _find_two_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u32, u32)> {
     for number in numbers {
@@ -29,7 +14,7 @@ fn _find_two_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u3
     None
 }
 
-fn _find_three_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u32, u32, u32)> {
+pub fn _find_three_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, u32, u32, u32)> {
     for number in numbers {
         let rest = sum_to - number;
         if let Some((number_b, number_c, product)) = _find_two_that_sum_to(&numbers, rest) {
@@ -43,7 +28,23 @@ fn _find_three_that_sum_to(numbers: &HashSet<u32>, sum_to: u32) -> Option<(u32, 
 
 #[cfg(test)]
 mod tests {
-    use crate::exercises::day1::{_find_two_that_sum_to, _read_input, _find_three_that_sum_to};
+    use crate::exercises::day1::{_find_two_that_sum_to, _find_three_that_sum_to};
+    use std::collections::HashSet;
+    use std::fs::File;
+    use std::io::Read;
+
+    fn _read_input() -> HashSet<u32> {
+        let mut file = File::open("src/exercises/day1/input.txt").expect("File exists and can be opened");
+        let mut contents = String::new();
+        file.read_to_string(&mut contents).expect("Can read and is valid text");
+
+        let mut numbers = HashSet::default();
+        for input in contents.lines() {
+            numbers.insert(str::parse::<u32>(input).expect("It is a u32"));
+        }
+
+        numbers
+    }
 
     #[test]
     fn day_1a_works() {
